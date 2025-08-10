ec190555a9ff4f2e528c31eea22040290d22f75a
Pasaa2025 <kerolosmonir27@gmail.com>
2025-08-10 11:39:10 +0300

    more examples using fluent validation package

--- SurveyBasket/Contracts/Validations/CreatePollRequestValidator.cs
+++ SurveyBasket/Contracts/Validations/CreatePollRequestValidator.cs
@@ -7,7 +7,15 @@ public class CreatePollRequestValidator  : AbstractValidator<CreatePollRequest>
     {
         RuleFor(e => e.Title)
             .NotEmpty()
-            .WithMessage("Title should not be null or empty");
+            .WithMessage("{PropertyName} should not be null or empty")
+            .Length(3, 20)
+            .WithMessage("{PropertyName} should be between {MinLength} and {MaxLength} character , you entered just [{TotalLength}] char");
+
+        RuleFor(e => e.Description)
+           .NotEmpty()
+           .WithMessage("{PropertyName} should not be null or empty")
+           .Length(10, 1000)
+           .WithMessage("{PropertyName} should be between {MinLength} and {MaxLength} character , you entered just [{TotalLength}] char");
 
     }
 }
--- SurveyBasket/Entities/Student.cs
+++ SurveyBasket/Entities/Student.cs
@@ -8,7 +8,6 @@ public class Student
     public string FirstName { get; set; } = string.Empty;
     public string MiddleName {  get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    [MinAge(18)]
     [Display(Name = "Date Of Birth")]
     public DateTime? DateOfBirth { get; set; }
     public Department Department { get; set; } = default!;
